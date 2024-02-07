04718b34866967438b1007a05e3ac7aa681708c6
tinmanSimon <s234wu@uwaterloo.ca>
2024-02-07 12:43:13 -0800

    can get product IDs now

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import requestUtils
-res = requestUtils.makeRequest("GET", "/api/v3/brokerage/products")
-print(res.read())
+import json
+
+def getProductsIDs(filename = "tmp"):
+    res = requestUtils.makeRequest("GET", "/api/v3/brokerage/products")
+    if res.getcode() != 200: return
+    response = json.loads(res.read().decode())
+    products = response["products"]
+    return [p["product_id"] for p in products]
+
+getProductsIDs()
