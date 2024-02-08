765ca1b415288b8ea310e5469fff42dd2aa97306
tinmanSimon <s234wu@uwaterloo.ca>
2024-02-07 23:26:56 -0800

    now can request different versions of coinbase api

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import requestUtils
 import json
+from collections import Counter
 
-def getProductsIDs(filename = "tmp"):
-    res = requestUtils.makeRequest("GET", "/api/v3/brokerage/products")
-    if res.getcode() != 200: return
-    response = json.loads(res.read().decode())
-    products = response["products"]
-    return [p["product_id"] for p in products]
+coinbaseMainDomain = "api.coinbase.com"
+coinbaseExchangeDomain = "api.exchange.coinbase.com"
 
-getProductsIDs()
+# this only gets the product ids that are ending with USD.
+def getProductsIDs():
+    res = requestUtils.makeRequest('GET', coinbaseMainDomain, '/api/v3/brokerage/products')
+    if res.status_code != 200: return []
+    response = res.json()
+    products = response['products']
+    return [p['product_id'] for p in products if p['product_id'].split('-')[-1] == 'USD']
+
+def getCandles(productId):
+    #todo, construct path using productId, and calculate time, and granularity
+    path = "/products/BTC-USD/candles?granularity=3600&start=1707120017&end=1707206417"
+    res = requestUtils.makeRequest('GET', coinbaseExchangeDomain, path)
+    if res.status_code != 200: return []
+    print(res.json())
+
+productIds = getProductsIDs()
+getCandles('BTC-USD')
--- requestUtils.py
+++ requestUtils.py
@@ -1,15 +1,19 @@
 
 from coinbase import jwt_generator
 from coinbase.constants import USER_AGENT
-import http.client
+import requests
 
 # mysecrets is not uploaded for security reasons. it just contains api_key and api_secret.
 from mysecrets import api_key, api_secret
 
-def getJWT(request_method, request_path):
-    jwt_uri = jwt_generator.format_jwt_uri(request_method, request_path)
-    jwt_token = jwt_generator.build_rest_jwt(jwt_uri, api_key, api_secret)
-    return jwt_token
+# this is using the default base url to generate jwt uri. check format_jwt_uri for more
+def getJWT(method, path):
+    jwt_uri = jwt_generator.format_jwt_uri(method, path)
+    return jwt_generator.build_rest_jwt(jwt_uri, api_key, api_secret)
+
+def getJWT(method, apiDomain, path):
+    jwt_uri = f"{method} {apiDomain}{path}"
+    return jwt_generator.build_rest_jwt(jwt_uri, api_key, api_secret)
 
 def getCBHeaders(jwt_token):
     return {
@@ -19,16 +23,12 @@ def getCBHeaders(jwt_token):
     }
 
 # returns response. use res.getcode() to get request code, use res.read() to get the response data.
-def makeRequest(method, path, payload = ''):
-    jwt_token = getJWT(method, path)
+def makeRequest(method, apiDomain, path):
+    jwt_token = getJWT(method, apiDomain, path)
     headers = getCBHeaders(jwt_token)
-    conn = http.client.HTTPSConnection("api.coinbase.com")
-    conn.request(method, path, payload, headers)
-    res = conn.getresponse()
-    if res.getcode() != 200:
-        # error handling
-        print("Error for makeRequest of", method, path, "errorCode:", res.getcode())
-        print("Err body: ", res.read())
-    return res
-
-
+    url = "https://" + apiDomain + path
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print("Error for makeRequest of", method, path, "errorCode:", response.status_code)
+        print("Err body: ", response.text)
+    return response
